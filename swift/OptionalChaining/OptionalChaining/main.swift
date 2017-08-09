6c7bd7bb5cc8c95b3e931d075d95eb180c06085a
tiger <guotenghu@gmail.com>
2017-08-09 22:42:11 +0800

    swift optional chaining

---
+++ swift/OptionalChaining/OptionalChaining/main.swift
@@ -0,0 +1,179 @@
+//
+//  main.swift
+//  OptionalChaining
+//
+//  Created by huahuahu on 2017/8/9.
+//  Copyright © 2017年 huahuahu. All rights reserved.
+//
+
+import Foundation
+
+
+//
+//class Person {
+//    var residence: Residence?
+//}
+//
+//class Residence {
+//    var numberOfRooms = 1
+//}
+//
+//let john = Person()
+//
+////let roomCount = john.residence!.numberOfRooms
+////// this triggers a runtime error
+//
+//if let roomCount = john.residence?.numberOfRooms {
+//    print("John's residence has \(roomCount) room(s).")
+//} else {
+//    print("Unable to retrieve the number of rooms.")
+//}
+//// Prints "Unable to retrieve the number of rooms."
+//
+//john.residence = Residence()
+//
+////roomCount 的类型是 Int?
+//if let roomCount = john.residence?.numberOfRooms {
+//    print("John's residence has \(roomCount) room(s).")
+//} else {
+//    print("Unable to retrieve the number of rooms.")
+//}
+//// Prints "John's residence has 1 room(s)."
+//
+//--------------------------------------------------------------
+
+class Person {
+    var residence: Residence?
+}
+
+class Room {
+    let name: String
+    init(name: String) { self.name = name }
+}
+
+class Address {
+    var buildingName: String?
+    var buildingNumber: String?
+    var street: String?
+    func buildingIdentifier() -> String? {
+        if let buildingNumber = buildingNumber, let street = street {
+            return "\(buildingNumber) \(street)"
+        } else if buildingName != nil {
+            return buildingName
+        } else {
+            return nil
+        }
+    }
+}
+
+class Residence {
+    var rooms = [Room]()
+    var numberOfRooms: Int {
+        return rooms.count
+    }
+    subscript(i: Int) -> Room {
+        get {
+            return rooms[i]
+        }
+        set {
+            rooms[i] = newValue
+        }
+    }
+    func printNumberOfRooms() {
+        print("The number of rooms is \(numberOfRooms)")
+    }
+    var address: Address?
+}
+
+let john = Person()
+if let roomCount = john.residence?.numberOfRooms {
+    print("John's residence has \(roomCount) room(s).")
+} else {
+    print("Unable to retrieve the number of rooms.")
+}
+// Prints "Unable to retrieve the number of rooms."
+
+let someAddress = Address()
+someAddress.buildingNumber = "29"
+someAddress.street = "Acacia Road"
+//下面会失败，因为 residence 是nil
+john.residence?.address = someAddress
+
+func createAddress() -> Address {
+    print("Function was called.")
+    
+    let someAddress = Address()
+    someAddress.buildingNumber = "29"
+    someAddress.street = "Acacia Road"
+    
+    return someAddress
+}
+//createAddress() 没有执行，因为左边直接失败了
+john.residence?.address = createAddress()
+
+
+// printNumberOfRooms 返回值是 Void。通过可选链调用，最终返回值是 Void?
+if john.residence?.printNumberOfRooms() != nil {
+    print("It was possible to print the number of rooms.")
+} else {
+    print("It was not possible to print the number of rooms.")
+}
+// Prints "It was not possible to print the number of rooms."
+
+
+//Any attempt to set a property through optional chaining returns a value of type Void?,
+if (john.residence?.address = someAddress) != nil {
+    print("It was possible to set the address.")
+} else {
+    print("It was not possible to set the address.")
+}
+// Prints "It was not possible to set the address."
+
+
+if let firstRoomName = john.residence?[0].name {
+    print("The first room name is \(firstRoomName).")
+} else {
+    print("Unable to retrieve the first room name.")
+}
+// Prints "Unable to retrieve the first room name."
+
+john.residence?[0] = Room(name: "Bathroom")
+
+let johnsHouse = Residence()
+johnsHouse.rooms.append(Room(name: "Living Room"))
+johnsHouse.rooms.append(Room(name: "Kitchen"))
+john.residence = johnsHouse
+
+if let firstRoomName = john.residence?[0].name {
+    print("The first room name is \(firstRoomName).")
+} else {
+    print("Unable to retrieve the first room name.")
+}
+// Prints "The first room name is Living Room."
+
+
+var testScores = ["Dave": [86, 82, 84], "Bev": [79, 94, 81]]
+testScores["Dave"]?[0] = 91
+testScores["Bev"]?[0] += 1
+testScores["Brian"]?[0] = 72
+// the "Dave" array is now [91, 82, 84] and the "Bev" array is now [80, 94, 81]
+
+if let johnsStreet = john.residence?.address?.street {
+    print("John's street name is \(johnsStreet).")
+} else {
+    print("Unable to retrieve the address.")
+}
+// Prints "Unable to retrieve the address."
+
+let johnsAddress = Address()
+johnsAddress.buildingName = "The Larches"
+johnsAddress.street = "Laurel Street"
+john.residence?.address = johnsAddress
+
+if let johnsStreet = john.residence?.address?.street {
+    print("John's street name is \(johnsStreet).")
+} else {
+    print("Unable to retrieve the address.")
+}
+// Prints "John's street name is Laurel Street."
+
