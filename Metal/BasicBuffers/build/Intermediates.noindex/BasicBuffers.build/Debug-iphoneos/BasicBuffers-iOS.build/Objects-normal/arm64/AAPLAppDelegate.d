6318f28b807512f106f97be2b408fe11377da7a2
tiger <guotenghu@gmail.com>
2017-09-30 22:12:16 +0800

    basic buffer metal

---
+++ Metal/BasicBuffers/build/Intermediates.noindex/BasicBuffers.build/Debug-iphoneos/BasicBuffers-iOS.build/Objects-normal/arm64/AAPLAppDelegate.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/huahuahu/GitHub/learn/Metal/BasicBuffers/iOS/AAPLAppDelegate.m \
+  /Users/huahuahu/GitHub/learn/Metal/BasicBuffers/iOS/AAPLAppDelegate.h
---
+++ Metal/BasicBuffers/build/Intermediates.noindex/BasicBuffers.build/Debug-iphoneos/BasicBuffers-iOS.build/Objects-normal/arm64/AAPLRenderer.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/huahuahu/GitHub/learn/Metal/BasicBuffers/Shared/AAPLRenderer.m \
+  /Users/huahuahu/GitHub/learn/Metal/BasicBuffers/Shared/AAPLRenderer.h \
+  /Users/huahuahu/GitHub/learn/Metal/BasicBuffers/Shared/AAPLShaderTypes.h
