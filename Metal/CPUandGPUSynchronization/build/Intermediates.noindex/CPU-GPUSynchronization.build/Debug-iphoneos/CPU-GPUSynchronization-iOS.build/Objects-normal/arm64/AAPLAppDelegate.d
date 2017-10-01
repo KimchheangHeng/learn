b6430546e55e60102dd3e21053802b030bb45f7c
tiger <guotenghu@gmail.com>
2017-10-01 09:50:58 +0800

    metal CPU  GPU sync

---
+++ Metal/CPUandGPUSynchronization/build/Intermediates.noindex/CPU-GPUSynchronization.build/Debug-iphoneos/CPU-GPUSynchronization-iOS.build/Objects-normal/arm64/AAPLAppDelegate.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/huahuahu/GitHub/learn/Metal/CPUandGPUSynchronization/iOS/AAPLAppDelegate.m \
+  /Users/huahuahu/GitHub/learn/Metal/CPUandGPUSynchronization/iOS/AAPLAppDelegate.h
