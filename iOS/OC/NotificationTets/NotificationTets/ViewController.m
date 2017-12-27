0bd316682dd0f7990d118d441e7a5cf8950f3576
tiger <guotenghu@gmail.com>
2017-12-27 12:57:16 +0800

    为通知demo加注释

--- iOS/OC/NotificationTets/NotificationTets/ViewController.m
+++ iOS/OC/NotificationTets/NotificationTets/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSString * const kNotifyName = @"kNotifyName";
+    //一个类可以为一个通知注册两个方法。两个方法都会被回调。
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(f1) name:kNotifyName object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(f2) name:kNotifyName object:nil];
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
