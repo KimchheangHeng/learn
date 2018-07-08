09f41d3590be0e08c2b4f30799773d45149a365b
guotenghu <guotenghu@bytedance.com>
2018-07-08 20:54:16 +0800

    dynamic fonts, change layout for different contentsize
    Change-Id: Ic7f8106c048126cbb2205fbd92a626fc36661532

--- iOS/UIKit/DynamicTypeDemo/DynamicTypeDemo/LayoutViewController.swift
+++ iOS/UIKit/DynamicTypeDemo/DynamicTypeDemo/LayoutViewController.swift
@@ -11,14 +11,38 @@ import UIKit
 class LayoutViewController: UIViewController {
     var onelineConstraint: [NSLayoutConstraint] = []
     var twolineConstraint: [NSLayoutConstraint] = []
+    var isCurrentAccessibility = false
 
     @IBOutlet weak var firstLabel: UILabel!
     @IBOutlet weak var secondLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        firstLabel.translatesAutoresizingMaskIntoConstraints = false
+        secondLabel.translatesAutoresizingMaskIntoConstraints = false
+        firstLabel.setContentHuggingPriority(UILayoutPriority.init(751), for: .vertical)
+        
+        twolineConstraint = [firstLabel.firstBaselineAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: UIFontMetrics.default.scaledValue(for: 100)),
+                             firstLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor, constant: 0),
+                             secondLabel.centerXAnchor.constraint(equalTo: firstLabel.centerXAnchor, constant: 0),
+                             secondLabel.firstBaselineAnchor.constraintEqualToSystemSpacingBelow(firstLabel.lastBaselineAnchor, multiplier: 1)]
+
+        onelineConstraint = [
+            firstLabel.firstBaselineAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: UIFontMetrics.default.scaledValue(for: 200) ),
+            firstLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            firstLabel.trailingAnchor.constraint(equalTo: secondLabel.leadingAnchor, constant: 10),
+            secondLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            secondLabel.firstBaselineAnchor.constraint(equalTo: firstLabel.firstBaselineAnchor, constant: 0)]
 
         self.layoutForSize(UIApplication.shared.preferredContentSizeCategory)
+        isCurrentAccessibility = UIApplication.shared.preferredContentSizeCategory.isAccessibilityCategory
+        if isCurrentAccessibility {
+            layoutForTwoLine()
+        } else {
+            layoutForOneline()
+        }
         // Do any additional setup after loading the view.
         NotificationCenter.default.addObserver(forName: .UIContentSizeCategoryDidChange, object: .none, queue: OperationQueue.main) { (notify) in
             let size = notify.userInfo![UIContentSizeCategoryNewValueKey] as! UIContentSizeCategory
@@ -32,15 +56,34 @@ class LayoutViewController: UIViewController {
     }
 
     func layoutForSize(_ size: UIContentSizeCategory) -> Void {
-
+        
+        if isCurrentAccessibility != size.isAccessibilityCategory {
+            isCurrentAccessibility = size.isAccessibilityCategory
+            firstLabel.removeConstraints(firstLabel.constraints)
+            secondLabel.removeConstraints(secondLabel.constraints)
+            if isCurrentAccessibility {
+                layoutForTwoLine()
+            } else {
+                layoutForOneline()
+            }
+            view.setNeedsLayout()
+        }
     }
 
     func layoutForOneline() -> Void {
-        firstLabel.remove
+        firstLabel.textAlignment = .left
+        secondLabel.textAlignment = .right
+        NSLayoutConstraint.deactivate(twolineConstraint)
+        NSLayoutConstraint.activate(onelineConstraint)
+        print("layout oneline")
     }
 
     func layoutForTwoLine() -> Void {
-        <#function body#>
+        firstLabel.textAlignment = .center
+        secondLabel.textAlignment = .center
+        NSLayoutConstraint.deactivate(onelineConstraint)
+        NSLayoutConstraint.activate(twolineConstraint)
+        print("layout twoline")
     }
 
 
